82ec60608101561af377fdd1e4d0f53fa20ec681
Javos <ram16230@uvg.edu.gt>
2019-09-16 09:56:56 -0600

    TODO fix D

--- simulation.py
+++ simulation.py
@@ -5,8 +5,8 @@ import random
 INF = sys.maxsize
 
 
-def generateTime(a, s):
-    return s - (1/a) * math.log(random.random())
+def generateTime(a):
+    return - (1/a) * math.log(random.random())
 
 
 def simulation(
@@ -20,8 +20,8 @@ def simulation(
     Na = 0  # Numero de llegadas al sistema
     C = [0] * servers  # Clientes servidos por el i-esimo server
     n = 0  # clientes en el sistema
-    i = [INF] * servers  # cliente que es servido por el i-esimo server
-    T = [generateTime(a, t)]  # generar la primera llegada
+    i = [0] * servers  # cliente que es servido por el i-esimo server
+    T = [generateTime(a)]  # generar la primera llegada
     tA = T[0]  # la siguiente llegada
     tD = [INF] * servers  # el tiempo de la siguiente completacion del i-esimo server
     # ---------------------------------
@@ -32,17 +32,21 @@ def simulation(
     D = {}
 
     while True:
+        print(t)
         # cases
-        if tA == min([tA] + tD):  # if tA is the next event
+        if tA == min([tA] + tD) and tA < Tn:  # if tA is the next event
+            # print("First case")
             t = tA
             Na = Na + 1
-            T.append(generateTime(a, t))
-            tA = T[-1]
+            to = t + generateTime(a)
+            T.append(to)
+            tA = to
+            
             A[Na] = t
             if n == 0 and all(v == 0 for v in i):  # if it is the first event
                 n = 1
                 i[0] = Na
-                Y = generateTime(a, t)
+                Y = generateTime(a)
                 tD[0] = t + Y
             # if there are less clients  than servers
             if n < servers:
@@ -51,35 +55,56 @@ def simulation(
                     if server == 0:  # is empty
                         n = n + 1
                         i[index] = Na
-                        Y = generateTime(a, t)
+                        Y = generateTime(a)
                         tD[index] = t + Y
                         break
             # there is no space
             else:
                 # add to wait
                 n = n + 1
-        else:
-            for index, ti in enumerate(tD):
-                if t == min([tA] + tD):  # if ti is the next event
-                    t = ti
-                    C[index] = C[index] + 1
-                    D[index] = t
-                    if n <= servers:  # if there are less clients on sistem than servers
-                        for j, server in enumerate(i):
-                            if n == j:
-                                n = n - 1
-                                i[j] = 0
-                                tD[index] = INF
-                                break
-                    else:
-                        m = max(i)
-                        n = n - 1
-                        i[index] = m + 1
-                        Y = generateTime(t)
-                        tD = y + Y
+        for index, ti in enumerate(tD):
+            if ti == min([tA] + tD) and ti < Tn:  # if ti is the next event
+                # print("Second case")
+                t = ti
+                C[index] = C[index] + 1
+                D[index] = t
+                if n <= servers:  # if there are less clients on sistem than servers
+                    for j, server in enumerate(i):
+                        if n == j:
+                            n = n - 1
+                            i[j] = 0
+                            tD[index] = INF
+                            break
+                else:
+                    m = max(i)
+                    n = n - 1
+                    i[index] = m + 1
+                    Y = generateTime(a)
+                    tD[index] = t + Y
 
+        if min([tA] + tD) > Tn and n > 0:
+            print("Third case")
+            todo = [tA] + tD
+            td = min(todo)
+            nxt = tD.index(min(tD))  # id of i server to finish
+            t = td
+            n = n - 1
+            i[nxt] = max(i) + 1
+            if n > 0:
+                Y = generateTime(a)
+                tD[nxt] = t + Y
+                D[nxt] = t
 
+        if min([tA] + tD) > Tn and n == 0:
+            print("Fourth case")
+            m = min(tD)
+            t = m
+            Tp = max(t - Tn, 0)
+            return A, D, Tp, t
 
+A, D, Tp, t = simulation(10, 2000, 40)
 
-
-                    
+print(len(A))
+print(len(D))
+print(D)
+print(t)
